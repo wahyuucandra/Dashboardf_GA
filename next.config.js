1f491037a171688f3a6a77845949fab7d8e7e1dc
mahmadakbar <mahmadakbar@live.com>
2023-12-13 17:46:14 +0700

    add: applu PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  //   disable: process.env.NODE_ENV === "development",
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = withPWA(nextConfig)
---
+++ src/assets/images/index.tsx
@@ -0,0 +1,6 @@
+// const API = process.env.NEXT_PUBLIC_API_URL
+const images = {
+  LOGO_BERIJALAN: '/img/logoBerijalan.png',
+}
+
+export default images
