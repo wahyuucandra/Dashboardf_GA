a4c1380ef57aed70ce33c6eae64ad16e3b47dfa3
Azriel Setyo <86305687+Stay2607@users.noreply.github.com>
2024-07-11 22:54:21 +0700

    add: Menambahkan modal logout untuk halaman profile

--- src/components/molecules/Profile/Account/index.tsx
+++ src/components/molecules/Profile/Account/index.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
+import { Modal } from '@components/atoms/ModalCustom'
 import IconChevronLeft from '@assets/icons/IconChevronLeft'
 import IconChevronRight from '@assets/icons/IconChevronRight'
 import IconHistory from '@assets/icons/IconHistory'
 import IconLogout from '@assets/icons/IconLogout'
 import IconNotification from '@assets/icons/IconNotification'
+import IconModalLogout from '@assets/icons/IconModalLogout'
 
 export function Account() {
   const router = useRouter()
+  const [isConfimationModalOpen, setIsConfimationModalOpen] = useState<boolean>(false)
 
   return (
     <div className="bg-[#f6f6f6] px-3 pt-2 h-full w-full">
@@ -51,13 +55,51 @@ export function Account() {
         </div>
         <IconChevronRight width={16} height={16} />
       </div>
-      <div className="flex flex-row items-center bg-white px-4 py-4 justify-between rounded-md">
+      <div
+        className="flex flex-row items-center bg-white px-4 py-4 justify-between rounded-md"
+        onClick={() => {
+          setIsConfimationModalOpen(true)
+        }}
+        onKeyDown={() => {}}
+      >
         <div className="flex flex-row items-center">
           <IconLogout width={24} height={24} color="red"></IconLogout>
           <p className="font-normal text-[14px] ml-2 text-red-700">Log out</p>
         </div>
         <IconChevronRight width={16} height={16} />
       </div>
+      <Modal isOpen={isConfimationModalOpen} backdropClick={() => setIsConfimationModalOpen(!isConfimationModalOpen)}>
+        <div className="max-w-[350px] bg-white relative p-6 text-center rounded-xl">
+          <div>
+            <IconModalLogout className="mx-auto mb-4 w-28 h-28" />
+          </div>
+          <div className="text-heading s semibold-18 text-[#252525] mb-1">Yakin ingin logout?</div>
+          <div className="text-paragraph regular-14 text-[#717171] px-3">Anda akan keluar dari akun ini.</div>
+          <div className="text-paragraph regular-14 text-[#717171] mb-8 px-3">Apakah Anda yakin ingin melanjutkan?</div>
+
+          <div className="grid grid-cols-2 gap-4 justify-items-center">
+            <button
+              onClick={() => {
+                setIsConfimationModalOpen(false)
+                router.push(`/login`, { scroll: false })
+              }}
+              type="button"
+              className="exit-button w-full text-center text-[#00376A] rounded-md overflow-hidden h-11"
+            >
+              <div className="py-2.5 px-6 text-heading xs semibold-16 text-red">Log Out</div>
+            </button>
+            <button
+              onClick={() => {
+                setIsConfimationModalOpen(false)
+              }}
+              type="button"
+              className="cancel-button w-full text-center text-white rounded-xl overflow-hidden h-11"
+            >
+              <div className="py-2.5 px-6 text-heading xs semibold-16">Kembali</div>
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
