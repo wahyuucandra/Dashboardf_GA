36306615bc3ffbf5c76e3a22494fa95290caca82
Abid Fakhri Maulana <abidfakhrimaulana@Abids-MacBook-Pro.local>
2024-07-12 08:06:22 +0700

    add vehicle security - v6

---
+++ src/app/booking-asset/vehicle-security/special-operational/vehicle-exit/page.tsx
@@ -0,0 +1,9 @@
+import { VehicleSecurityVehicleExitPage } from '@components/organisms/VehicleSecurity'
+
+export const metadata = {
+  title: 'Vehicle Security Exit Form',
+}
+
+export default function SpecialOperationalExit() {
+  return <VehicleSecurityVehicleExitPage></VehicleSecurityVehicleExitPage>
+}
--- src/components/organisms/VehicleSecurity/Schedule/index.tsx
+++
@@ -1,9 +0,0 @@
-import { VehicleSecuritySchedule } from '@components/molecules/VehicleSecurity'
-
-export function SchedulePage() {
-  return (
-    <>
-      <VehicleSecuritySchedule></VehicleSecuritySchedule>
-    </>
-  )
-}
