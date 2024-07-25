45f09ef8cc6aea33d8232563c1505261f4bc53f5
Abid Fakhri Maulana <abidfakhrimaulana@Abids-MacBook-Pro.local>
2024-07-25 09:55:51 +0700

    integration api booking asset module

--- src/interfaces/api.tsx
+++ src/interfaces/api.tsx
@@ -1,7 +1,16 @@
+export interface Pagination {
+  totalRecords: number
+  currentPage: number
+  totalPage: number
+  nextPage: number | null
+  prevPage: number | null
+}
+
 export interface APIBaseResponse<T = undefined> {
   reqId: string
   status: boolean | number | string
   message: string
   error: string | null
   data?: T extends undefined ? never : T
+  pagination?: Pagination | undefined
 }
