089ff26bded8f6863fcdb2992f6049540e946b37
Azriel Setyo <86305687+Stay2607@users.noreply.github.com>
2024-07-26 08:39:42 +0700

    add: menambahkan middleware dan juga nama di profile home page

--- src/components/molecules/Home/ProfileHeader/index.tsx
+++ src/components/molecules/Home/ProfileHeader/index.tsx
@@ -2,9 +2,11 @@
 
 import IconNotification from '@assets/icons/IconNotification'
 import { GetCookie } from '@store/storage'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export function ProfileHeader() {
+  const router = useRouter()
   const [dataUser, setDataUser] = useState<any>({})
 
   useEffect(() => {
@@ -17,10 +19,10 @@ export function ProfileHeader() {
       <div className="w-14 h-14 rounded-full bg-[#D9D9D9]"></div>
       <div className="flex-1 flex flex-col space-y-1">
         <span className="text-paragraph regular-14">Halo,</span>
-        <span className="text-heading s semibold-18">{dataUser?.data?.namaUser || '-'}</span>
+        <span className="text-heading s semibold-18">{dataUser?.nameUser || '-'}</span>
       </div>
-      <button>
-        <IconNotification></IconNotification>
+      <button onClick={() => router.push('/notification')}>
+        <IconNotification />
       </button>
     </div>
   )
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -2,23 +2,22 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const cookieAUth = request.cookies.get('access_token')
-  const response = NextResponse.next()
+  const cookieAuth = request.cookies.get('access_token')
 
   // Redirect unauthenticated users to login (except login and excluded paths)
-  if (!cookieAUth && !request.nextUrl.pathname.startsWith('/login')) {
+  if (!cookieAuth && !['/login', '/excluded-path'].includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
   // Redirect authenticated users to home if they try to access login
-  if (cookieAUth && request.nextUrl.pathname.startsWith('/login')) {
+  if (cookieAuth && request.nextUrl.pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  return response
+  return NextResponse.next()
 }
 
-// See "Matching Paths" below to learn more
+// Configure paths to include or exclude from the middleware
 export const config = {
   matcher: ['/((?!login|_next/static|_next/image|favicon.ico|img|icons|svg).*)'],
 }
