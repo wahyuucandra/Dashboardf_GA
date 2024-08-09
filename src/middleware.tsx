10d550e6ff952100c9ae0811688266fc00230abd
Azriel Setyo <86305687+Stay2607@users.noreply.github.com>
2024-08-09 11:08:28 +0700

    fix: Memperbaiki routing di building maintenance service

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -4,14 +4,15 @@ import type { NextRequest } from 'next/server'
 export function middleware (request: NextRequest) {
   const cookieAuth = request.cookies.get('access_token')
 
-  // Redirect unauthenticated users to login (except login and excluded paths)
-  if (!cookieAuth && !['/login', '/excluded-path'].includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  // // Redirect unauthenticated users to login (except login and excluded paths)
+  // if (!cookieAuth && !['/login', '/excluded-path'].includes(request.nextUrl.pathname)) {
+  //   return NextResponse.redirect(new URL('/login', request.url))
+  // }
 
-  // Redirect authenticated users to home if they try to access login
+  // Check if the user is authenticated and trying to access the login page
   if (cookieAuth && request.nextUrl.pathname.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/', request.url))
+    // Redirect authenticated users to the home page or another suitable page
+    return NextResponse.redirect(new URL('/', request.url)) // Or any other desired redirect path
   }
 
   return NextResponse.next()
