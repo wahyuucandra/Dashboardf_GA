cd5a17e493cdeb152ce23d0de9203aac85a3aa70
Azriel Setyo <86305687+Stay2607@users.noreply.github.com>
2024-08-14 15:01:49 +0700

    fix: Memperbaiki path yang di exclude di middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -17,8 +17,14 @@ export function middleware(request: NextRequest) {
     return response
   }
 
-  // Redirect unauthenticated users to login (except login and excluded paths)
-  if (!cookieAuth && !['/login', '/excluded-path'].includes(request.nextUrl.pathname)) {
+  // Redirect unauthenticated users to login (except paths starting with /login, /register, /forgot-password, and /excluded-path)
+  if (
+    !cookieAuth &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/register') &&
+    !request.nextUrl.pathname.startsWith('/forgot-password') &&
+    !request.nextUrl.pathname.startsWith('/excluded-path')
+  ) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
